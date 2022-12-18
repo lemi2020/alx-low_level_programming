e8489a446ac2d6adaf9cbb069929f82a21938e1a
lemi2020 <lemibelay2020@gmail.com>
2022-12-17 23:42:17 -0800

    yrddd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-puts "Programming is like building a multilingual"`/n 0
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
