c8062e06b6f90117f2426ba0e4df1528dde60a98
lemi2020 <lemibelay2020@gmail.com>
2022-12-19 06:53:26 -0800

    kedame tawate

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by new line..
+ *
+ * @str: the string to print.
+ */
+void _puts(char *str)
+{
+	int i;
+	for (i = 0; str[i] i= '\0'; i++)
+	{ 
+            _putchar(str[i]);
+	}
+         
+	_putchar(str[i]);
+}
+
